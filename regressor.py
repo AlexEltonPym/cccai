580d296c0949d982e69a786681b4a4145361a668
alexeltonpym <alexeltonpym@gmail.com>
2018-10-17 10:23:46 +1100

    1-4

--- regressor.py
+++ regressor.py
@@ -1,68 +1,123 @@
-#get rows, into big array
-#convert student number into array of 00000100000 and combine with same for challenges
-#fit that tree against output
-		#output is Y
-		#only fit 90% and save 10% for testing the fit
-#predict data not used
-
-
 import numpy as np
-from sklearn import tree
 from sklearn.ensemble import RandomForestRegressor
-
-
+from sklearn.metrics import mean_squared_error
 import csv
+import sys
 
-studentCount = 31
-challengeCount = 3
-questionCount = 10
+studentCount = 33
+questionCount = 15
+sampleCount = 125
 
-splitPercent = 0.7
+names = ['u%d' % i for i in range(studentCount)] + ['s0','s1','s2','s3','s4','s5','s6','s7','s8','s9','q0','q1','q2','q3','a0']
+
+user = "user"
+enjoy = "s0"
+skills = "s1"
+prepare = "s2"
+time = "s3"
+conscious = "s4"
+new = "s5"
+unexpected = "s6"
+learnt = "s7"
+better = "s8"
+motivated = "s9"
+quiz0 = "q0"
+quiz1 = "q1"
+quiz2 = "q2"
+quiz3 = "q3"
+assignment = "a0"
+
+tests = [
+	('Everything (but enjoyment) -> enjoyment', [user, skills, prepare, time, conscious, new, unexpected, learnt, better, motivated], [enjoy]),
+	('Flow -> enjoyment + motivation', [skills, prepare, time, conscious], [enjoy, motivated]),
+	('Epistemic curiosity -> enjoyment + motivation', [new, unexpected, learnt, better], [enjoy, motivated]),
+	('Everything (including motivated) -> motivated', [user, skills, prepare, time, conscious, new, unexpected, learnt, better, motivated], [motivated]),
+	('Quizzes -> assignment', [quiz0, quiz1, quiz2, quiz3], [assignment]),
+]
+
+percents = [0.7]
+
+def main():
+	dataset = loadDataset('mvp2.csv')
+
+	for test in tests:
+		for p in percents:
+			splitPoint = int(p * sampleCount)
+
+			samples = splitDataset(dataset, test, splitPoint)
+
+			results = runRegressor(samples)
+			printResults(results, samples[3], test[0])
 
 
-with open('mvp.csv', 'r') as csvfile:
-	reader = csv.DictReader(csvfile)
+def loadDataset(filename):
+	with open(filename, 'r') as csvfile:
+		reader = csv.DictReader(csvfile)
 
-	dataset = np.empty_like(dtype={'names':['q0','q1','q2','q3','q4','q5','q6','q7','q8','q9','u']})
+		dataset = np.zeros((sampleCount, studentCount + questionCount)) #initialise dataset array
 
-	for row in reader:
-		datum = []
+		i = 0
+		for row in reader:
+			datum = np.zeros(studentCount) #create user like this: 0000100000
+			datum[int(row['u'])] = 1
 
-		user = np.zeros(studentCount, dtype=np.int) #user 0000100000
-		user[int(row['u'])] = 1
-		datum.append(user)
+			questions = np.fromiter(map(float, list(row.values())[1:16]), dtype=np.int) #get question responses
+			datum = np.append(datum, questions) #add questions to sample array
+			dataset[i] = datum #add sample to dataset
+			i = i + 1
+		return dataset
 
-		datum.extend(map(int, list(row.values())[1:11])) #question responses 23415
+def splitDataset(dataset, test, splitPoint):
+	testName, x, y = test
 
-		dataset.append(np.array(datum))
-
-	splitPoint = int(splitPercent * len(dataset))
-	dataset.shuffle()
+	np.random.shuffle(dataset)
 
 	trainingData = dataset[:splitPoint]
 	validationData = dataset[splitPoint:]
 
-  
-
-	trainingX = [sample[:trainingSplit] for sample in dataset[:splitPoint]]
-	trainingY = [sample[trainingSplit:] for sample in dataset[:splitPoint]]
+	trainingX = trainingData[..., nums(x)]
+	trainingY = trainingData[..., nums(y)]
 	
-	validationX = [sample[:trainingSplit] for sample in dataset[splitPoint:]]
-	validationY = [sample[trainingSplit:] for sample in dataset[splitPoint:]]
+	validationX = validationData[..., nums(x)]
+	validationY = validationData[..., nums(y)]
+	return (trainingX, trainingY, validationX, validationY)
 
-	# regr = tree.DecisionTreeRegressor()
-	# regr.fit(trainingX, trainingY)
-
+def runRegressor(tup):
+	trainingX, trainingY, validationX, validationY = tup
 	regr = RandomForestRegressor()
-	regr.fit(trainingX, np.ravel(trainingY))
+
+	if len(trainingY[0]) == 1:
+		regr.fit(trainingX, np.ravel(trainingY))
+	else:
+		regr.fit(trainingX, trainingY)
 
 	prediction = regr.predict(validationX)
 	score = regr.score(validationX, validationY)
+	return prediction, score
 
-	# print(prediction)
-	# print(validationY)
 
-	print(score)
 
-	# print(list([int(pred) for pred in prediction]))
-	# print(list([int(correct[0]) for correct in validationY]))
+def printResults(results, answers, testName):
+	prediction, score = results
+	mse = mean_squared_error(answers, prediction)
+
+	print("Test: " + testName)
+	print("Training samples: " + str(sampleCount - len(answers)))
+	print("Validation samples: " + str(len(answers)))
+	print("Coefficient of determination: " + str(score))
+	print("Mean squared error: " + str(mse) + "\n")
+
+	#print(str(mse), file=sys.stderr)
+
+
+def nums(inputList):
+	ret = []
+
+	for name in inputList:
+		if(name is 'user'):
+			ret.extend(range(studentCount))
+		else:
+			ret.append(names.index(name))
+	return ret
+
+main()
